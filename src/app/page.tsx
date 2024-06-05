caf5564116b9c44cd9593c0db5c62c5eb6c8c6d4
jkwon-dev <kwonejk@gmail.com>
2024-06-05 17:12:26 +0900

    SideBar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,28 @@
+import FollowingBar from "@/components/FollowingBar";
+import PostList from "@/components/PostList";
+import SideBar from "@/components/SideBar";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return <h1 className="text-orange-700">Threestargram</h1>;
+export default async function HomePage() {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if (!user) {
+    redirect("/auth/signin");
+  }
+
+  return (
+    <section className="w-full flex flex-col md:flex-row max-w-[850px] p-4">
+      <div className="w-full basis-3/4">
+        <FollowingBar />
+        <PostList />
+      </div>
+      <div className="basis-1/4">
+        <SideBar user={user} />
+      </div>
+    </section>
+  );
 }
