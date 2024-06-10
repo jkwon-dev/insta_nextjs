679240c7537afb00169cb5ce2636e044a076e5b8
jkwon-dev <kwonejk@gmail.com>
2024-06-10 14:12:22 +0900

    PostList

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,3 +1,28 @@
+"use client";
+import { SimplePost } from "@/model/post";
+import { GridLoader } from "react-spinners";
+import useSWR from "swr";
+import PostListCard from "./PostListCard";
+
 export default function PostList() {
-  return <p>PostList</p>;
+  const { data: posts, isLoading: loading } =
+    useSWR<SimplePost[]>("/api/posts");
+  return (
+    <section>
+      {loading && (
+        <div className="text-center mt-32">
+          <GridLoader color="red" />
+        </div>
+      )}
+      {posts && (
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id} className="mb-4">
+              <PostListCard post={post} />
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
+  );
 }
