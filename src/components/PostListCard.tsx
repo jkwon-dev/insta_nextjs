8d6c7f3effaa0e37e62d999ceaf5ce60d476d555
jkwon-dev <kwonejk@gmail.com>
2024-06-11 15:53:02 +0900

    use Portal

--- src/components/PostListCard.tsx
+++ src/components/PostListCard.tsx
@@ -1,8 +1,12 @@
+"use client";
 import { SimplePost } from "@/model/post";
 import Avatar from "./Avatar";
 import Image from "next/image";
 import CommentForm from "./CommentForm";
 import ActionBar from "./ActionBar";
+import { useState } from "react";
+import ModalPortal from "./ModalPortal";
+import PostModal from "./PostModal";
 
 type Props = {
   post: SimplePost;
@@ -11,6 +15,7 @@ type Props = {
 
 export default function PostListCard({ post, priority = false }: Props) {
   const { userImage, username, image, createdAt, text, likes } = post;
+  const [openModal, setOpenModal] = useState(false);
   return (
     <article className="rounded-lg shadow-md border border-gray-200">
       <div className="flex items-center p-2">
@@ -24,6 +29,7 @@ export default function PostListCard({ post, priority = false }: Props) {
         width={500}
         height={500}
         priority={priority}
+        onClick={() => setOpenModal(true)}
       />
 
       <ActionBar
@@ -33,6 +39,13 @@ export default function PostListCard({ post, priority = false }: Props) {
         createdAt={createdAt}
       />
       <CommentForm />
+      {openModal && (
+        <ModalPortal>
+          <PostModal onClose={() => setOpenModal(false)}>
+            <p> Post Detailed page</p>
+          </PostModal>
+        </ModalPortal>
+      )}
     </article>
   );
 }
